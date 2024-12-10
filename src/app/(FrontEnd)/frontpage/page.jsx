4f4d395f0104a435bc412d208ecaae0afc2dda73
Kushal Bhatt <Kushalbhatt1016@gmail.com>
2024-12-10 21:02:00 +0530

    files create

---
+++ src/app/(FrontEnd)/frontpage/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <header/>
+      <h1>Kushal</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div>
+      <h1>Vaibhav</h1>
+    </div>
+  )
+}
+
+export default header
