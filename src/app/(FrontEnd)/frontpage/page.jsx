b056cda99f8c67701dd991d82d317a45b22b7197
Kushal Bhatt <Kushalbhatt1016@gmail.com>
2024-12-12 20:51:09 +0530

    Header and Footer

--- src/app/(FrontEnd)/frontpage/page.jsx
+++ src/app/(FrontEnd)/frontpage/page.jsx
@@ -1,11 +1,13 @@
 import Header from '@/app/components/header'
+import Footer from '@/app/components/footer'
 import React from 'react'
 
 function page() {
   return (
     <div>
       <Header />
-      <h1>Kushal</h1>
+      <h1>Body Content</h1>
+      <Footer/>
     </div>
   )
 }
